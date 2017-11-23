7071acb1db871a1f946d3eeb901e78ceee309ff6
Feng <feng@xps-1>
2017-11-23 21:49:28 +0100

    updated map_1st example

--- ptx/fxaudahpqbfwqg.cu
+++ ptx/fxaudahpqbfwqg.cu
@@ -9,10 +9,7 @@ gf_jsigstfmqkjxv(double* __restrict__  a, double* __restrict__  b, double* __res
 {
     unsigned long const const index = (blockDim.x * blockIdx.x + threadIdx.x);
     df_jsigstfmqkjxv( a[index],  b[index],  c[index]) ;
-    df_jsigstfmqkjxv( a[index+2560],  b[index+2560],  c[index+2560]) ;
-    df_jsigstfmqkjxv( a[index+5120],  b[index+5120],  c[index+5120]) ;
-    df_jsigstfmqkjxv( a[index+7680],  b[index+7680],  c[index+7680]) ;
-    if ( index < 871 )
-        df_jsigstfmqkjxv( a[index+10240],  b[index+10240],  c[index+10240]) ;
+    if ( index < 3431 )
+        df_jsigstfmqkjxv( a[index+7680],  b[index+7680],  c[index+7680]) ;
 }
 
